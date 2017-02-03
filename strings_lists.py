0b9619b200689222c38777a6d619b4ec5aaa76bc
chris <chris@nara>
2017-02-03 14:47:05 -0600

    save

---
+++ strings_lists.py
@@ -0,0 +1,55 @@
+# part 3 Practice with Lists
+#print "This creats a list of even numbers from 0 to input number."
+#number = int(raw_input("Please input a number:"))
+#evens = [n for n in range(number) if n%2==0]
+#print "evens list is: " + str(evens)
+
+#---------------------------------------------------
+
+# 2
+#print "This creats a list of numbers divisible by divisor up to number."
+#divisor = int(raw_input("Please input a divisor:"))
+#number = int(raw_input("Please input a number:"))
+#divisibles = [n for n in range(number) if n%divisor==0]
+#print "divisibles list is: " + str(divisibles)
+
+
+#---------------------------------------------------
+
+# # 3
+# print "This prints common elements from two lists."
+# l1 = [0, 3, 6, 9, 10, 2, 5]
+# l2 = [2, 6, 4, 7, 8, 1, 15]
+# common =[]
+# for i in l1:
+#     if i in l2:
+#         common.append(i)
+# common.sort()
+# print "The common elements from the 2 lists are: " + str(common)
+
+
+#---------------------------------------------------
+
+# 4
+print "This creats a list of multiples of first number up to max."
+multiple = int(raw_input("Please input a multiple:"))
+max = int(raw_input("Please input the max:"))
+multiples = [n for n in range(max) if n%multiple==0]
+print "multiples list is: " + str(multiples
+                                  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
